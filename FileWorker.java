591d14ea4a16099f1645426af24a53dbffdb386a
Лилия <l_loshakova123@mail.ru>
2023-01-06 16:18:01 +0300

    Sketch

--- FileWorker.java
+++ FileWorker.java
@@ -1,5 +1,14 @@
 package ZOO;
 
-public class FileWorker {
+public class FileWorker implements Weitable {
+    @Override
+    public void save(){
+        
+    }
+
+    @Override
+    public Object read(){
+        return null;
+    }
     
 }
